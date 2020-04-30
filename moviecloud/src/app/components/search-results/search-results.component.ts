cd1ba2a9738085782bd77f3f9e7f3d1581646559
Christopher Iliyas <christopheriliyas@gmail.com>
2020-04-30 13:45:34 +0100

    finish search component

--- moviecloud/src/app/components/search-results/search-results.component.ts
+++ moviecloud/src/app/components/search-results/search-results.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Movie } from '@app/models/Movie';
 import { TvShow } from '@app/models/TvShow';
 import { TopRatedService, UpcomingService, NowPlayingService, PopularService } from '@app/services/movies/movie.service';
@@ -7,16 +8,22 @@ import { TodayService, PopularTvService, OnAirService, TopRatedTvService } from 
 import { DiscoverService } from '@app/services/discover/discover.service';
 import { People } from '@app/models/People';
 import { tmdbConfig } from '@app/utils/constants';
+import { Genre } from '@app/models/Genre';
+import { GlobalService } from '@app/services/global/global.service';
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
   styleUrls: ['./search-results.component.scss']
 })
-export class SearchResultsComponent implements OnInit {
+export class SearchResultsComponent implements OnInit, OnDestroy {
   searchTerm: string;
   searchResults: Movie[] | TvShow[] | People[];
   page: number;
+  movieSubscription: Subscription;
+  tvSubscription: Subscription;
+  movieGenres: Genre[];
+  tvGenres: Genre[];
   tmdb: object = tmdbConfig;
 
   constructor(
@@ -29,7 +36,8 @@ export class SearchResultsComponent implements OnInit {
     private popularTv: PopularTvService,
     private onAir: OnAirService,
     private topTv: TopRatedTvService,
-    private discover: DiscoverService
+    private discover: DiscoverService,
+    private global: GlobalService
     ) { 
     this.searchResults = [];
     this.page = 1;
@@ -37,10 +45,19 @@ export class SearchResultsComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.movieSubscription = this.global.getEntity('movie_genre').subscribe(res => {
+      this.movieGenres = res.genres ? res.genres : [];
+    });
+  
+    this.tvSubscription = this.global.getEntity('tv_genre').subscribe(res => {
+      this.tvGenres = res.genres ? res.genres : [];
+    });
+
     this.route.paramMap.subscribe(res => {
       this.searchTerm = res.get('id');
       this.handleSearch(this.searchTerm, this.page);
     });
+
   }
 
   handleSearch(searchTerm: string, page: number) {
@@ -85,6 +102,7 @@ export class SearchResultsComponent implements OnInit {
         this.discover.search(searchTerm, page).subscribe(res => this.searchResults = res.results);
         break;
     }
+    window.scrollTo(0, 0);
   }
 
   parseSearchStr(searchTerm: string): string {
@@ -99,6 +117,24 @@ export class SearchResultsComponent implements OnInit {
   getPreviousPage() {
     this.page = this.page - 1;
     this.handleSearch(this.searchTerm, this.page);
+  }
+
+  handleGenres(itemType: string, genreIds: number[]): object {
+    const genreList = itemType == 'movie' ? this.movieGenres : this.tvGenres;
+    if (genreList) {
+      let genresArr = genreList.filter(genre => parseInt(genre.id, 10) == genreIds[0] || parseInt(genre.id, 10) == genreIds[1] ? genre.name : null);
+      return { 
+        name1: genresArr[0] ? genresArr[0].name : '',
+        name2: genresArr[1] ? ` / ` + genresArr[1].name : ''
+      };
+    } else {
+      return { name1: '', name2: '' };
+    }
+  }
+
+  ngOnDestroy() {
+    this.movieSubscription.unsubscribe();
+    this.tvSubscription.unsubscribe();
   }
 
 }
